ead6dd2182a27f1a0eacc0b4915115b0553d5825
kkamara <kamaracomputers@gmail.com>
2023-07-12 00:33:48 +0100

    Add project

---
+++ app/src/main/java/com/kelvinkamara/helloworld/MainActivity.java
@@ -0,0 +1,23 @@
+package com.kelvinkamara.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick(View vie) {
+        TextView txtHello = findViewById(R.id.txtMessage);
+        EditText editTxtName = findViewById(R.id.editTxtName);
+        txtHello.setText("Hello " + editTxtName.getText().toString());
+    }
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+![see-message-java-mobile.png](https://github.com/kkamara/useful/blob/main/see-message-java-mobile.png?raw=true)
+
+# see-message-java-mobile
+
+## Installation
+
+* [Android Studio Flamingo](https://nodejs.org/en/).
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[BSD](https://opensource.org/licenses/BSD-3-Clause)
